bebdcdbd1704f488060eb3900814d2ce9c8bc467
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-05 21:35:59 +0530

    REFACTORED: Separate method to calculate stock values

--- src/com/bridgelabz/stockaccountmanagement/StockAccountManagementMain.java
+++ src/com/bridgelabz/stockaccountmanagement/StockAccountManagementMain.java
@@ -2,6 +2,8 @@ package com.bridgelabz.stockaccountmanagement;
 
 public class StockAccountManagementMain {
 public static void main(String[] args) {
-	StockPortfolio.addStocks();
-}
+	StockPortfolio stockPortfolio = new StockPortfolio();
+	stockPortfolio.addStocks();
+	double totalValue = stockPortfolio.calculateValues();
+	System.out.println("Total value of all the stocks is: " + totalValue);}
 }
--- src/com/bridgelabz/stockaccountmanagement/StockPortfolio.java
+++ src/com/bridgelabz/stockaccountmanagement/StockPortfolio.java
@@ -6,32 +6,36 @@ import com.bridgelabz.linkedlist.MyLinkedList;
 import com.bridgelabz.linkedlist.MyNode;
 
 public class StockPortfolio {
-	private static MyLinkedList<Stock> stocksList = new MyLinkedList<Stock>();
+	private MyLinkedList<Stock> stocksList = new MyLinkedList<Stock>();
+	private double totalStockValue;
 
-	public static void addStocks() {
+	public void addStocks() {
 		System.out.println("Enter the number of Stocks that want to add: ");
 		Scanner scanner = new Scanner(System.in);
 		int count = scanner.nextInt();
-
-		double totalStockValue = 0;
-
-		for (int index = 0; index < count; index++) {
+		for(int index =0;index<count;index++) {
 			System.out.println("Enter the name of stock");
 			String name = scanner.next();
 			System.out.println("Enter the number of shares");
 			int numOfShares = scanner.nextInt();
 			System.out.println("Enter the price for each share");
 			double price = scanner.nextDouble();
-			Stock stock = new Stock(name, numOfShares, price);
-
-			double value = numOfShares * price;
-			System.out.println("Value for stock '" + name + "' is: " + value);
-			totalStockValue += value;
-
+			Stock stock = new Stock(name,numOfShares,price);
 			MyNode<Stock> myStockNode = new MyNode<Stock>(stock);
 			stocksList.append(myStockNode);
 		}
-		System.out.println("Total stock value = "+totalStockValue);
 		scanner.close();
+	}
+	
+	public double calculateValues() {
+		MyNode<Stock> tempNode = (MyNode<Stock>) stocksList.head;
+		while(tempNode != null) {
+			Stock stock = tempNode.getKey();
+			double value = stock.getNumberOfShares() * stock.getSharePrice();
+			System.out.println("Value for stock '" + stock.getName() + "' is: " + value);
+			totalStockValue += value;
+			tempNode = (MyNode<Stock>)tempNode.getNext();
+		}
+		return totalStockValue;
 	}
 }
